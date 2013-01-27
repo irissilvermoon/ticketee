b1ab1fb5f80af074a1fdbf0a60b211b8a06cba44
Karen Caplan <keaslk@gmail.com>
2013-01-27 12:25:59 -0800

    passing editing projects tests.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -21,6 +21,17 @@ class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:id])
   end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    @project.update_attributes(params[:project])
+    flash[:notice] = "Project has been updated."
+    redirect_to @project
+  end
 end
 
 
