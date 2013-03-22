12c52fb5561ef7747eee1d5af366914d9f7ee29e
Karen Caplan <keaslk@gmail.com>
2013-03-21 22:23:44 -0700

    Add receiver class to receive emails

--- spec/mailers/notifier_spec.rb
+++ spec/mailers/notifier_spec.rb
@@ -25,5 +25,10 @@ describe Notifier do
       email.body.should include("#{comment.user.email} wrote:")
       email.body.should include(comment.text)
     end
+
+    it "correctly sets the Reply-To" do
+      address = "youraccount+#{project.id}+#{ticket.id}@gmail.com"
+      email.reply_to.should == [address]
+    end
   end
 end
